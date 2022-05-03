729d8aa7121ea88b3f5c90b27e25da6ddeaadacb
Anthony Romero-Jorge <AnthonyRomero@MacBook-Pro.local>
2022-05-03 06:02:20 -0700

    added Medium challenges

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//EASY 1
 let newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const myPrimeArray = newArray.filter((num) => {
@@ -9,3 +9,34 @@ const myPrimeArray = newArray.filter((num) => {
   return num !== 1;
 });
 console.log(myPrimeArray);
+
+//MEDIUM 1
+
+const vowelChecker = (str) => {
+  if (str === "a" || str === "A") {
+    console.log("This is a vowel");
+  } else if (str === "e" || str === "E") {
+    console.log("This is a vowel");
+  } else if (str === "i" || str === "I") {
+    console.log("This is a vowel");
+  } else if (str === "o" || str === "O") {
+    console.log("This is a vowel");
+  } else if (str === "u" || str === "U") {
+    console.log("This is a vowel");
+  } else {
+    console.log("This is not a vowel");
+  }
+};
+
+vowelChecker("A");
+
+//MEDIUM 3
+
+const gcd = (a, b) => {
+  if (a == 0) return b;
+  return gcd(b % a, a);
+};
+
+console.log(gcd(84, 9));
+
+//Hard
